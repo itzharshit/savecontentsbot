e5f0581419530532fe422d62065a1934e571dc5e
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-02-01 10:51:59 +0530

    Update _start.py

--- main/plugins/_start.py
+++ main/plugins/_start.py
@@ -8,6 +8,8 @@ from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
+st = "**Hii,\nI am @Pyrogrammers Save restricted Contents bot.**\nSend me any public or private restricted Channel post link.\nI will give you that post.\n**Hit /help to know more.**"
+
 @Drone.on(events.callbackquery.CallbackQuery(data="set"))
 async def sett(event):    
     Drone = event.client                    
@@ -42,7 +44,25 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")                        
   
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
+@Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await start_srb(event)
+    await event.reply(f'{st}', 
+                      buttons=[
+                        [Button.url("Updates Channel", url="https://t.me/pyrogrammers"),
+                         Button.url("Support Group", url="https://t.me/+7ScFy39Vckk5MWQ1")],
+                        [Button.url("YouTube Channel", url="https://youtube.com/channel/UC2anvk7MNeNzJ6B4c0SZepw")],
+                    ])
+    try:
+        await Bot.start()
+        await idle()
+    except Exception as e:
+        if 'Client is already connected' in str(e):
+            pass
+        else:
+            return
     
+       # start help Message
+@Drone.on(events.NewMessage(pattern="^/help$"))
+async def search(event):
+    await event.reply('<b><u>For Public Restricted Channel contents.</b></u>\nTo get public restricted Channel contents, just send your Post link i will give you that post without Downloading.\n\n<b><u>For Private Restricted Channel contents.</b></u>\nTo get private restricted Channel contents, First send me Channel invite link so that i can join your channel after that send me post link of your restricted Channel to get that post.', parse_mode="HTML")
+    #end help Message
